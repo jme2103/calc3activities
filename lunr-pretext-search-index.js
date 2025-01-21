48889faaca657429eb0dd2225083a9f8eaf2de69
jme2103 <43256224+jme2103@users.noreply.github.com>
2025-01-21 22:04:34 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -25,16 +25,214 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "2.1",
   "title": "Functions of Several Variables and Three Dimensional Space",
-  "body": " Functions of Several Variables and Three Dimensional Space  Text of section.   Activity 1.1.3  Which is true of the two coordinate systems shown above?   "
+  "body": " Functions of Several Variables and Three Dimensional Space    A function of two independent variables function of two variables is a rule that assigns to each ordered pair in some set exactly one real number .      The domain function domain of a function is the set of all inputs at which the function is defined.      Consider the concentration, C, (in mg\/liter) of a drug in the blood as a function of the amount of drug given, , and the time since injection, . For mg and hours, we have    Which of the following best describes the meaning of in this context?  is the concentration in the blood 3 hours after a 2 mg injection.  is the concentration in the blood 2 hours after a 3 mg injection.  is the change in concentration of a 3 mg dose 2 hours after injection.  is the change in concentration of a 2 mg does 3 hours after injection.       A car rental company charges a one-time application fee of 30 dollars, 50 dollars per day, and 11 cents per mile for its cars.    Write a formula for the cost, , of renting a car as a function of the number of days, , and the number of miles driven, .    Describe the domain of the function.      Which of the following best describes the domain of ?    The domain is all such that  The domain is all except the origin .  The domain is all such that  The domain is all .  The domain is all such that and because sine is not defined for negative numbers.       Identify the domain of the functions below.                                      Use technology to graph the function . Report the domain and the range of the function.      Identify the domain of each of the following functions. Draw a picture of each domain in the -plane.                             Describe a physical or real-world example of a function of two variables. What is its domain?    Describe a physical or real-world example of a function of three variables. What is its domain?      A three-dimensional coordinate system is called right-handed if, after pointing your right thumb in the direction of the positive -axis, your remaining fingers curl (as if you are making a fist) in the direction from the positive -axis to the positive -axis. Otherwise, it's called left-handed.      insert picture   Which of the following best describes the two coordinate systems?  The first one is right-handed, while the second is left-handed.  The first one is left-handed, while the second is right-handed.  They are both right-handed.  They are both left-handed.       The graph function graph of a function is the set of points of the form , where the point is in the domain of .      A trace trace of a function of two independent variables and in the direction is a curve of the form , where is a constant. Similarly, a trace of a function of two independent variables and in the direction is a curve of the form , where is a constant.      A level curve level curve (or contour) of a function of two independent variables and is a curve of the form , where is a constant.      Which of the following sets up the vertical trace for for the function ?                            Which of the following is a contour map for the function ?                                    Sketch the contour map for the function for      Which of the following best describes the difference between traces and contours?  There is no difference; they are the same.  Traces are \"vertical\" while contours are \"horizontal.\"  Traces never involve the coordinate, while contours do.  Contours never involve the coordinate, while traces do.       A sphere sphere is the set of all points equidistant from a fixed point. The fixed point is called the center of the sphere, and the distance from the center to any point on the sphere is called the radius of the sphere, and the distance from the center to all the points in the sphere is called the radius .      Find the equation of the sphere is center with radius . Normalize your equation so that the coefficient of is .    Give an equation which describes the intersection of this sphere with the plane .     Quadric Surface   A quadric surface is a surface of the form       There are six common types of quadric surfaces:   Ellipsoid     Elliptic Cone     Elliptic Paraboloid     Hyperboloid of One Sheet     Hyperboloid of Two Sheets     Hyperbolic Paraboloid          Each equation below is a quadric surface. Identify which kind it is.        Ellipsoid    Hyperboloid of One Sheet    Elliptic Cone    Elliptic Paraboloid    Hyperboloid of Two Sheets    Hyperbolic Paraboloid           Ellipsoid    Hyperboloid of One Sheet    Elliptic Cone    Elliptic Paraboloid    Hyperboloid of Two Sheets    Hyperbolic Paraboloid           Ellipsoid    Hyperboloid of One Sheet    Elliptic Cone    Elliptic Paraboloid    Hyperboloid of Two Sheets    Hyperbolic Paraboloid       "
+},
+{
+  "id": "sec-Functions-Of-Several-Vars-and-3D-Space-2",
+  "level": "2",
+  "url": "sec-Functions-Of-Several-Vars-and-3D-Space.html#sec-Functions-Of-Several-Vars-and-3D-Space-2",
+  "type": "Definition",
+  "number": "2.1.1",
+  "title": "",
+  "body": "  A function of two independent variables function of two variables is a rule that assigns to each ordered pair in some set exactly one real number .   "
 },
 {
   "id": "sec-Functions-Of-Several-Vars-and-3D-Space-3",
   "level": "2",
   "url": "sec-Functions-Of-Several-Vars-and-3D-Space.html#sec-Functions-Of-Several-Vars-and-3D-Space-3",
-  "type": "Example",
+  "type": "Definition",
+  "number": "2.1.2",
+  "title": "",
+  "body": "  The domain function domain of a function is the set of all inputs at which the function is defined.   "
+},
+{
+  "id": "activity-n1-se1",
+  "level": "2",
+  "url": "sec-Functions-Of-Several-Vars-and-3D-Space.html#activity-n1-se1",
+  "type": "Activity",
   "number": "2.1.1",
-  "title": "Activity 1.1.3.",
-  "body": " Activity 1.1.3  Which is true of the two coordinate systems shown above?  "
+  "title": "",
+  "body": "  Consider the concentration, C, (in mg\/liter) of a drug in the blood as a function of the amount of drug given, , and the time since injection, . For mg and hours, we have    Which of the following best describes the meaning of in this context?  is the concentration in the blood 3 hours after a 2 mg injection.  is the concentration in the blood 2 hours after a 3 mg injection.  is the change in concentration of a 3 mg dose 2 hours after injection.  is the change in concentration of a 2 mg does 3 hours after injection.    "
+},
+{
+  "id": "activity-n1-fb1",
+  "level": "2",
+  "url": "sec-Functions-Of-Several-Vars-and-3D-Space.html#activity-n1-fb1",
+  "type": "Activity",
+  "number": "2.1.2",
+  "title": "",
+  "body": "  A car rental company charges a one-time application fee of 30 dollars, 50 dollars per day, and 11 cents per mile for its cars.    Write a formula for the cost, , of renting a car as a function of the number of days, , and the number of miles driven, .    Describe the domain of the function.   "
+},
+{
+  "id": "activity-n1-se2",
+  "level": "2",
+  "url": "sec-Functions-Of-Several-Vars-and-3D-Space.html#activity-n1-se2",
+  "type": "Activity",
+  "number": "2.1.3",
+  "title": "",
+  "body": "  Which of the following best describes the domain of ?    The domain is all such that  The domain is all except the origin .  The domain is all such that  The domain is all .  The domain is all such that and because sine is not defined for negative numbers.    "
+},
+{
+  "id": "pd-functions-domain-range1",
+  "level": "2",
+  "url": "sec-Functions-Of-Several-Vars-and-3D-Space.html#pd-functions-domain-range1",
+  "type": "Activity",
+  "number": "2.1.4",
+  "title": "",
+  "body": "  Identify the domain of the functions below.                                   "
+},
+{
+  "id": "pd-functions-graph1",
+  "level": "2",
+  "url": "sec-Functions-Of-Several-Vars-and-3D-Space.html#pd-functions-graph1",
+  "type": "Activity",
+  "number": "2.1.5",
+  "title": "",
+  "body": "  Use technology to graph the function . Report the domain and the range of the function.   "
+},
+{
+  "id": "activit-n1-fb",
+  "level": "2",
+  "url": "sec-Functions-Of-Several-Vars-and-3D-Space.html#activit-n1-fb",
+  "type": "Activity",
+  "number": "2.1.6",
+  "title": "",
+  "body": "  Identify the domain of each of the following functions. Draw a picture of each domain in the -plane.                          "
+},
+{
+  "id": "activity-n1-fe",
+  "level": "2",
+  "url": "sec-Functions-Of-Several-Vars-and-3D-Space.html#activity-n1-fe",
+  "type": "Activity",
+  "number": "2.1.7",
+  "title": "",
+  "body": "  Describe a physical or real-world example of a function of two variables. What is its domain?    Describe a physical or real-world example of a function of three variables. What is its domain?   "
+},
+{
+  "id": "sec-Functions-Of-Several-Vars-and-3D-Space-11",
+  "level": "2",
+  "url": "sec-Functions-Of-Several-Vars-and-3D-Space.html#sec-Functions-Of-Several-Vars-and-3D-Space-11",
+  "type": "Definition",
+  "number": "2.1.3",
+  "title": "",
+  "body": "  A three-dimensional coordinate system is called right-handed if, after pointing your right thumb in the direction of the positive -axis, your remaining fingers curl (as if you are making a fist) in the direction from the positive -axis to the positive -axis. Otherwise, it's called left-handed.   "
+},
+{
+  "id": "activity-n1-se3",
+  "level": "2",
+  "url": "sec-Functions-Of-Several-Vars-and-3D-Space.html#activity-n1-se3",
+  "type": "Activity",
+  "number": "2.1.8",
+  "title": "",
+  "body": "  insert picture   Which of the following best describes the two coordinate systems?  The first one is right-handed, while the second is left-handed.  The first one is left-handed, while the second is right-handed.  They are both right-handed.  They are both left-handed.    "
+},
+{
+  "id": "sec-Functions-Of-Several-Vars-and-3D-Space-13",
+  "level": "2",
+  "url": "sec-Functions-Of-Several-Vars-and-3D-Space.html#sec-Functions-Of-Several-Vars-and-3D-Space-13",
+  "type": "Definition",
+  "number": "2.1.4",
+  "title": "",
+  "body": "  The graph function graph of a function is the set of points of the form , where the point is in the domain of .   "
+},
+{
+  "id": "sec-Functions-Of-Several-Vars-and-3D-Space-14",
+  "level": "2",
+  "url": "sec-Functions-Of-Several-Vars-and-3D-Space.html#sec-Functions-Of-Several-Vars-and-3D-Space-14",
+  "type": "Definition",
+  "number": "2.1.5",
+  "title": "",
+  "body": "  A trace trace of a function of two independent variables and in the direction is a curve of the form , where is a constant. Similarly, a trace of a function of two independent variables and in the direction is a curve of the form , where is a constant.   "
+},
+{
+  "id": "sec-Functions-Of-Several-Vars-and-3D-Space-15",
+  "level": "2",
+  "url": "sec-Functions-Of-Several-Vars-and-3D-Space.html#sec-Functions-Of-Several-Vars-and-3D-Space-15",
+  "type": "Definition",
+  "number": "2.1.6",
+  "title": "",
+  "body": "  A level curve level curve (or contour) of a function of two independent variables and is a curve of the form , where is a constant.   "
+},
+{
+  "id": "pd-vertical-trace1",
+  "level": "2",
+  "url": "sec-Functions-Of-Several-Vars-and-3D-Space.html#pd-vertical-trace1",
+  "type": "Activity",
+  "number": "2.1.9",
+  "title": "",
+  "body": "  Which of the following sets up the vertical trace for for the function ?                         "
+},
+{
+  "id": "sec-Functions-Of-Several-Vars-and-3D-Space-17",
+  "level": "2",
+  "url": "sec-Functions-Of-Several-Vars-and-3D-Space.html#sec-Functions-Of-Several-Vars-and-3D-Space-17",
+  "type": "Activity",
+  "number": "2.1.10",
+  "title": "",
+  "body": "  Which of the following is a contour map for the function ?                                 "
+},
+{
+  "id": "sec-Functions-Of-Several-Vars-and-3D-Space-18",
+  "level": "2",
+  "url": "sec-Functions-Of-Several-Vars-and-3D-Space.html#sec-Functions-Of-Several-Vars-and-3D-Space-18",
+  "type": "Activity",
+  "number": "2.1.11",
+  "title": "",
+  "body": "  Sketch the contour map for the function for    "
+},
+{
+  "id": "activity-n1-fe3",
+  "level": "2",
+  "url": "sec-Functions-Of-Several-Vars-and-3D-Space.html#activity-n1-fe3",
+  "type": "Activity",
+  "number": "2.1.12",
+  "title": "",
+  "body": " Which of the following best describes the difference between traces and contours?  There is no difference; they are the same.  Traces are \"vertical\" while contours are \"horizontal.\"  Traces never involve the coordinate, while contours do.  Contours never involve the coordinate, while traces do.    "
+},
+{
+  "id": "sec-Functions-Of-Several-Vars-and-3D-Space-20",
+  "level": "2",
+  "url": "sec-Functions-Of-Several-Vars-and-3D-Space.html#sec-Functions-Of-Several-Vars-and-3D-Space-20",
+  "type": "Definition",
+  "number": "2.1.7",
+  "title": "",
+  "body": "  A sphere sphere is the set of all points equidistant from a fixed point. The fixed point is called the center of the sphere, and the distance from the center to any point on the sphere is called the radius of the sphere, and the distance from the center to all the points in the sphere is called the radius .   "
+},
+{
+  "id": "sec-Functions-Of-Several-Vars-and-3D-Space-21",
+  "level": "2",
+  "url": "sec-Functions-Of-Several-Vars-and-3D-Space.html#sec-Functions-Of-Several-Vars-and-3D-Space-21",
+  "type": "Activity",
+  "number": "2.1.13",
+  "title": "",
+  "body": "  Find the equation of the sphere is center with radius . Normalize your equation so that the coefficient of is .    Give an equation which describes the intersection of this sphere with the plane .   "
+},
+{
+  "id": "def-quadric-surface",
+  "level": "2",
+  "url": "sec-Functions-Of-Several-Vars-and-3D-Space.html#def-quadric-surface",
+  "type": "Definition",
+  "number": "2.1.8",
+  "title": "Quadric Surface.",
+  "body": " Quadric Surface   A quadric surface is a surface of the form    "
+},
+{
+  "id": "fact-quadric-surfaces",
+  "level": "2",
+  "url": "sec-Functions-Of-Several-Vars-and-3D-Space.html#fact-quadric-surfaces",
+  "type": "Fact",
+  "number": "2.1.9",
+  "title": "",
+  "body": "  There are six common types of quadric surfaces:   Ellipsoid     Elliptic Cone     Elliptic Paraboloid     Hyperboloid of One Sheet     Hyperboloid of Two Sheets     Hyperbolic Paraboloid       "
+},
+{
+  "id": "sec-Functions-Of-Several-Vars-and-3D-Space-24",
+  "level": "2",
+  "url": "sec-Functions-Of-Several-Vars-and-3D-Space.html#sec-Functions-Of-Several-Vars-and-3D-Space-24",
+  "type": "Activity",
+  "number": "2.1.14",
+  "title": "",
+  "body": "  Each equation below is a quadric surface. Identify which kind it is.        Ellipsoid    Hyperboloid of One Sheet    Elliptic Cone    Elliptic Paraboloid    Hyperboloid of Two Sheets    Hyperbolic Paraboloid           Ellipsoid    Hyperboloid of One Sheet    Elliptic Cone    Elliptic Paraboloid    Hyperboloid of Two Sheets    Hyperbolic Paraboloid           Ellipsoid    Hyperboloid of One Sheet    Elliptic Cone    Elliptic Paraboloid    Hyperboloid of Two Sheets    Hyperbolic Paraboloid      "
 },
 {
   "id": "backmatter-2",
